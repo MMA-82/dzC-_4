a7461bff4eec7eb959e42f9511947b19b4075b91
Marat <brutto061@gmail.com>
2022-10-15 18:37:31 +0700

    Выполнили задание №6

---
+++ zadanie_6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задайте длину массива: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[length];
+Console.Write("Массив чисел: ");
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+for(int i = 0; i < array.Length/2; i++)
+{
+    int j = length - i - 1;
+    if(j>=i) Console.Write($"{array[i]} x {array[j]} = {array[i] * array[j]}");
+    Console.WriteLine();
+}
