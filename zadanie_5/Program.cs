2b9455eebe75da89a8ce698bd0b46cce24ba552c
Marat <brutto061@gmail.com>
2022-10-15 16:55:56 +0700

    Выполнили задание №5

---
+++ zadanie_5/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[15];
+Console.Write("Массив чисел: ");
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+int sumnechet = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] %2 ==1) sumnechet = sumnechet + array[i];
+}
+Console.WriteLine("Сумма нечетных чисел в массиве: " + sumnechet);
+
