38ae390f59ff966033896d238e4f5cc17fc49b40
Marat <brutto061@gmail.com>
2022-10-15 16:38:35 +0700

    Выполнили задание №3

---
+++ zadanie_3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[20];
+Console.Write("Массив трехзначных чисел: ");
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(99, 1000);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+int chet = 0;
+int nechet = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] %2 ==0) chet++;
+    else nechet++;
+}
+Console.WriteLine("Количество четных чисел в массиве: " + chet);
+Console.WriteLine("Количество нечетных чисел в массиве: " + nechet);
