66450c4f860cb573cf1470a4f979fcd8c3e6fe73
Marat <brutto061@gmail.com>
2022-10-15 16:49:31 +0700

    Выполнили задание №4

---
+++ zadanie_4/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[123];
+Console.Write("Массив из 123-х чисел: ");
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 1000);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+int result = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] >=10 && array[i] <=99) result++;
+}
+Console.WriteLine("Количество чисел, удовлетворяющих промежутку [10, 99]: " + result);
+
